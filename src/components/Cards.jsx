874b13dee568b3296c4761c030875af7e78d0eed
ayush <ayush@email.com>
2023-05-05 18:59:37 +0530

    card animation framer

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,20 @@
 import images from "../assets/Images"
+import { AnimatePresence, motion } from "framer-motion"
+
+function PrintImage() {
+	return images.map((image, i) => (
+		<motion.div
+			key={i}
+			initial={{ opacity: 0,scale:0}}
+			animate={{ opacity: 1,scale:1}}
+			transition={{delay:0.5 + i, type:"tween"}}
+		>
+			<motion.img src={image.link} alt={image.link} />
+		</motion.div>
+	))
+}
 
 export default function Cards() {
-	function PrintImage() {
-		return images.map((image, i) => (
-			<div key={i} className="card">
-				<img src={image.link} alt={image.link} />
-			</div>
-		))
-	}
-
 	return (
 		<div className="body">
 			<div className="gallery">
