39edae24195feb22f6f18cd9775f75f8e1e93400
ayush <ayush@email.com>
2023-05-05 20:52:06 +0530

    fix

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import images from "../assets/Images"
-import { AnimatePresence, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 function PrintImage() {
 	return images.map((image, i) => (
@@ -7,7 +7,7 @@ function PrintImage() {
 			key={i}
 			initial={{ opacity: 0,scale:0}}
 			animate={{ opacity: 1,scale:1}}
-			transition={{delay:0.5 + i, type:"tween"}}
+			transition={{delay:0.2 + i, type:"tween", ease:"easeIn"}}
 		>
 			<motion.img src={image.link} alt={image.link} />
 		</motion.div>
