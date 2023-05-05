9fbc8961feaa043a0e7d5a7a295a130ef3026343
ayush <ayush@email.com>
2023-05-05 19:33:07 +0530

    footer tweaks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Bottom from './footerComps/Bottom.jsx'
 
 export default function Footer() {
 	return (
-		<footer className="bg-white dark:bg-gray-900">
+		<footer className="footer">
 			<div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
 				<Top />
 				<hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
