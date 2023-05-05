b2cf095ddcee0722033860e2d5a3b87bf997448c
ayush <ayush@email.com>
2023-05-05 21:47:02 +0530

    headless navbar and svg animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Cards from "./components/Cards"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Example from "./components/NavBar"
 
 function App() {
 	return <div className="website">
-      <Header />
+      {/* <Header /> */}
+      <Example />
       <Cards />
       <Footer />
     </div>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,108 @@
+import { Disclosure } from "@headlessui/react"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import { motion } from "framer-motion"
+
+const navigation = [
+	{ name: "Images", href: "#", current: true },
+	{ name: "More", href: "#", current: false },
+	{ name: "Contact", href: "#", current: false },
+]
+
+const myTransition = { duration: 2, yoyo: Infinity, ease: "easeInOut" }
+
+function classNames(...classes) {
+	return classes.filter(Boolean).join(" ")
+}
+
+export default function Example() {
+	return (
+		<Disclosure as="nav" className="bg-gray-800">
+			{({ open }) => (
+				<>
+					<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+						<div className="relative flex h-16 items-center justify-between">
+							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+								{/* Mobile menu button*/}
+								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+									<span className="sr-only">Open main menu</span>
+									{open ? (
+										<XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+									) : (
+										<Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+									)}
+								</Disclosure.Button>
+							</div>
+
+							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+								<div className="flex flex-shrink-0 items-center">
+									<motion.svg
+										animate={{ scale: 1 }}
+										transition={myTransition}
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-10 h-10 stroke-2 stroke-white hover:stroke-1"
+									>
+										<motion.path
+											initial={{ pathLength: 0 }}
+											animate={{ pathLength: 1 }}
+											transition={myTransition}
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+										/>
+									</motion.svg>
+									<h1 className="text-xl font-sans text-white font-semibold">
+										CamTrap
+									</h1>
+								</div>
+								<div className="hidden sm:ml-6 sm:block">
+									<div className="flex space-x-4">
+										{navigation.map((item) => (
+											<a
+												key={item.name}
+												href={item.href}
+												className={classNames(
+													item.current
+														? "bg-gray-900 text-white"
+														: "text-gray-300 hover:bg-gray-700 hover:text-white",
+													"rounded-md px-3 py-2 text-sm font-medium"
+												)}
+												aria-current={item.current ? "page" : undefined}
+											>
+												{item.name}
+											</a>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<Disclosure.Panel className="sm:hidden">
+						<div className="space-y-1 px-2 pb-3 pt-2">
+							{navigation.map((item) => (
+								<Disclosure.Button
+									key={item.name}
+									as="a"
+									href={item.href}
+									className={classNames(
+										item.current
+											? "bg-gray-900 text-white"
+											: "text-gray-300 hover:bg-gray-700 hover:text-white",
+										"block rounded-md px-3 py-2 text-base font-medium"
+									)}
+									aria-current={item.current ? "page" : undefined}
+								>
+									{item.name}
+								</Disclosure.Button>
+							))}
+						</div>
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
+	)
+}
