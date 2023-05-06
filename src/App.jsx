c4cde490b90a986944ddfa376c7fe032a7d73b39
ayush <ayush@email.com>
2023-05-06 23:58:22 +0530

    animation update footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import Cards from "./components/Cards"
 import Footer from "./components/Footer"
-import Header from "./components/Header"
 import Example from "./components/NavBar"
 
 function App() {
 	return <div className="website">
-      {/* <Header /> */}
       <Example />
       <Cards />
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,15 @@ export default function Footer() {
 	return (
 		<footer className="footer">
 			<motion.div
-				initial={{ scale: 0, y: 200 }}
-				animate={{ scale: 1, y: 0 }}
-				transition={{ type: "tween", ease: "easeInOut", duration: 0.5 }}
+				initial={{ opacity: 0, x: -200 }}
+				animate={{ opacity: 1, x: 0 }}
+				transition={{
+					type: "spring",
+					ease: "easeInOut",
+					damping: 10,
+					stiffness: 100,
+					duration: 0.2,
+				}}
 				className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8"
 			>
 				<Top />
