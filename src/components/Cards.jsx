6a746f091da07dc51001f9461664058c78c21284
ayush <ayush.chauhan0008@gmail.com>
2023-05-18 00:54:13 +0530

    fetching from external api

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,36 @@
-import images from "../assets/Images"
+import { useEffect, useState } from "react"
 import { motion } from "framer-motion"
 
+const url = import.meta.env.VITE_URL
+
 function PrintImage() {
-	return images.map((image, i) => (
+	const [allImages, setAllImages] = useState([])
+
+	useEffect(() => {
+		// api fetching function
+		async function fetchUsers() {
+			const response = await fetch(url)
+			if (response.status === 200) {
+				const data = await response.json()
+				setAllImages(data)
+			} else {
+				throw new Error(`Request failed with status code ${response.status}`)
+			}
+		}
+		fetchUsers()
+	}, [])
+
+	return allImages.map((image, i) => (
 		<motion.div
 			key={i}
 			initial={{ opacity: 0, scale: 0 }}
 			animate={{ opacity: 1, scale: 1 }}
-			transition={{ delay: i * 0.5, type: "spring", ease: "easeInOut", stiffness:40 }}
+			transition={{
+				delay: i * 0.5,
+				type: "spring",
+				ease: "easeInOut",
+				stiffness: 40,
+			}}
 		>
 			<motion.img src={image.link} alt={image.link} />
 		</motion.div>
